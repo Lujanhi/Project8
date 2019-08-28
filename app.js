ae2f9c7673c5e5c807cf4dbbd4b102626b2ed59f
Lujanhi <51241524+Lujanhi@users.noreply.github.com>
2019-08-28 16:24:12 -0400

    save 5

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const Config = require('./config/config');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.redirect('/books'));
+
+app.use('/books', require('./routes/books'));
+
+app.use((req, res, next) => {
+    res.locals.error = err;
+    console.log('Error status:', err.status);
+    res.status(err.status || 500);
+    if (err.status === 404) {
+        res.render('page-not-found')
+    } else {
+        res.render('error')
+    }
+});
+
+Config.sync()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => console.log('Application running on localhost:3000'))
+    })
---
+++ config/config.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize({
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: './library.db'
+});
+
